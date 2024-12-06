263fb402fbdd4a24f7953e6eac6aa2b830dbee53
Pedro Botega <botegapedro@gmail.com>
2024-12-06 18:14:49 +0000

    Moves route file into root

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import styles from './App.module.scss';
-import { RoutesPages } from './routes/Routes';
+import { RoutesPages } from './Routes';
 
 function App() {
   return (
-      <div className={styles.App}>
-        {/* <NavbarModule/>
+    <div className={styles.App}>
+      {/* <NavbarModule/>
         <hr/>
         <div className={styles.dottedspaced}/> */}
-        <RoutesPages/>
-      </div>
+      <RoutesPages />
+    </div>
   );
 }
 
